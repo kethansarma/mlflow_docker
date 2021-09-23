38089f51e370d5d7c277edd40d3dc6aef6aa6455
kethansarma <kethansarma@gmail.com>
2021-09-23 21:04:44 +0530

    Added mlflow update command to workflow

--- train.py
+++ train.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     alpha = float(args.alpha)
     l1_ratio = float(args.l1_ratio)
 
-    with mlflow.start_run():
+    with mlflow.start_run() as run:
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
